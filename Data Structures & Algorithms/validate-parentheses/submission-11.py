f98493f7c50ea9b007a3232e086028eba177b1b9
Naravazhi <66281756+Naravazhi@users.noreply.github.com>
2026-04-06 15:21:57 -0400

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,125 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        char_hash = {")":"(", "}":"{", "]":"["}
+
+
+        for c in s:
+            if c == ")" or c == "}" or c== "]":
+                if stack and stack[-1] == char_hash[c]:
+                    stack.pop()
+                    continue
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if len(stack) == 0 else False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # stack = []
+        # pairs = {")":"(", "]":"[", "}":"{"}
+        # for ch in s:
+        #     if ch in pairs:
+        #         if not stack or stack[-1] != pairs[ch]:
+        #             return False
+        #         stack.pop()
+        #     elif ch in "{[(":
+        #         stack.append(ch)
+        #     else:
+        #         return False
+        # return True if len(stack) == 0 else False
+        
+        # stack = []
+        # for i in range(len(s)):
+        #     if s[i] == "}":
+        #         if stack:
+        #             if stack[-1] == "{":
+        #                 stack.pop()
+        #             else:
+        #                 return False
+        #         else:
+        #             return False
+        #     elif s[i] == "]":
+        #         if stack:
+        #             if stack[-1] == "[":
+        #                 stack.pop()
+        #             else:
+        #                 return False
+        #         else:
+        #             return False
+        #     elif s[i] == ")":
+        #         if stack:
+        #             if stack[-1] == "(":
+        #                 stack.pop()
+        #             else:
+        #                 return False
+        #         else:
+        #             return False
+        #     else:
+        #         stack.append(s[i])
+
+        # return True if len(stack) == 0 else False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # stack = []
+
+        # for char in s:
+        #     if char == ")":
+        #         if stack and stack[-1] == "(":
+        #             stack.pop()
+        #         else:
+        #             return False
+        #     elif char == "]":
+        #         if stack and stack[-1] == "[":
+        #             stack.pop()
+        #         else:
+        #             return False
+        #     elif char == "}":
+        #         if stack and stack[-1] == "{":
+        #             stack.pop()
+        #         else:
+        #             return False
+                    
+        #     else:
+        #         stack.append(char)
+
+        # return len(stack) == 0 
