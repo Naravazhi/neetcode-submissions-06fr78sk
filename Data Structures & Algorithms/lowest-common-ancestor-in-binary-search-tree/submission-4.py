3cb9924211453d62b85321a5fc14695ea4c39c0b
Naravazhi <66281756+Naravazhi@users.noreply.github.com>
2026-04-13 10:19:48 -0400

    Add: lowest-common-ancestor-in-binary-search-tree - submission-4

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-4.py
@@ -0,0 +1,54 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: Optional[TreeNode], p: Optional[TreeNode], q: Optional[TreeNode]) -> TreeNode:
+        def dfs(node):
+            if not node:
+                return None
+            if p.val < node.val and q.val < node.val:
+                return dfs(node.left)
+            elif p.val > node.val and q.val > node.val:
+                return dfs(node.right)
+            else:
+                return node
+        return dfs(root)
+
+            
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def lowestCommonAncestor(self, root: Optional[TreeNode], p: Optional[TreeNode], q: Optional[TreeNode]) -> TreeNode:
+
+
+#         def dfs(node):
+#             if not node:
+#                 return None
+#             if p.val < node.val and q.val < node.val:
+#                 return dfs(node.left)
+#             elif p.val > node.val and q.val > node.val:
+#                 return dfs(node.right)
+#             else:
+#                 return node
+#         return dfs(root)
+
+
+
+
+
+
+# class Solution:
+#     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        
