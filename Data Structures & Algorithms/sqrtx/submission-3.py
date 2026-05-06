23d52f47c5ed73b49dbe0c8cafa205b3f8208774
Naravazhi <66281756+Naravazhi@users.noreply.github.com>
2026-05-05 23:18:32 -0400

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.py
@@ -0,0 +1,67 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+
+        left = 0
+        right = x // 2
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            
+            if mid * mid > x:
+                right = mid - 1
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                return mid
+            # if mid * mid == x:
+            #     return mid
+            # elif mid * mid > x:
+            #     right = mid - 1
+            # else:
+            #     left = mid + 1
+
+        return left - 1
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+#         if x == 0:
+#             return 0
+#         if x == 1:
+#             return 1
+
+#         halfed = x // 2
+
+#         left = 1
+#         right = halfed
+
+#         while left <= right:
+#             mid = left + (right - left) // 2
+#             if mid * mid == x:
+#                 return mid
+#             elif mid * mid > x:
+#                 right = mid - 1
+#             else:
+#                 left = mid + 1
+#         return left - 1
