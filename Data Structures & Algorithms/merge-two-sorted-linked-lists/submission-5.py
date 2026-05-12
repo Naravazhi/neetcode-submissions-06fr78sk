fb3b7b80aa07fd61cdc837a8d3b2e459bc6f56c1
Naravazhi <66281756+Naravazhi@users.noreply.github.com>
2026-05-12 14:34:05 -0400

    Add: merge-two-sorted-linked-lists - submission-5

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-5.py
@@ -0,0 +1,105 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        curr = dummy
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+        return dummy.next
+
+
+
+
+
+
+
+
+
+
+
+        # ptr1 = list1
+        # ptr2 = list2
+        # dummy = ListNode(None)
+        # curr = dummy
+
+        # while ptr1 and ptr2:
+        #     if ptr1.val <= ptr2.val:
+        #         curr.next = ptr1
+        #         ptr1 = ptr1.next
+        #     else:
+        #         curr.next = ptr2
+        #         ptr2 = ptr2.next
+        #     curr = curr.next
+
+        # if ptr1:
+        #     curr.next = ptr1
+        # if ptr2:
+        #     curr.next = ptr2
+        # return dummy.next
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if not list1:
+        #     return list2
+
+        # if not list2:
+        #     return list1
+
+
+        # s1, s2 = list1, list2
+        # dummy = ListNode()
+        # curr = dummy
+
+        # while s1 and s2:
+        #     if s1.val <= s2.val:
+        #         curr.next = s1
+        #         s1 = s1.next
+
+        #     else:
+        #         curr.next = s2
+        #         s2 = s2.next
+
+        #     curr = curr.next
+
+
+
+        # if s1:
+        #     curr.next = s1
+
+        # if s2:
+        #     curr.next = s2
+
+        # return dummy.next
